e398da6ae25a6821e25f7cd953c8688b0750c03a
Orionick <72335975+Orionick@users.noreply.github.com>
2022-12-17 00:56:38 +0200

    Addedfiles

---
+++ main.cpp
@@ -0,0 +1,100 @@
+﻿#include <iostream>
+#include <vector>
+#include <fstream>
+#include <Windows.h>
+
+using namespace std;
+
+bool isUpper(char c) {
+	return c >= 'А' && c <= 'Я';
+}
+
+bool isLower(char c) {
+	return c >= 'а' && c <= 'я';
+}
+
+char toUpper(char c) {
+	if (isLower(c)) {
+		return c - 32;
+	}
+	return c;
+}
+
+char toLower(char c) {
+	if (isUpper(c)) {
+		return c + 32;
+	}
+	return c;
+}
+
+bool isLetter(char c) {
+	return isUpper(c) || isLower(c);
+}
+
+void write(vector<char>& word, vector<char>& foundLetters, ofstream& outputFile) {
+	if (word.size() > 0) {
+		for (size_t i = 0; i < word.size() - 1; i++) {
+			char left = toLower(word[i]);
+			char right = toLower(word[i + 1]);
+
+			if (left == right) {
+				word[i] = toUpper(left);
+				word[i + 1] = toUpper(right);
+
+				if (find(foundLetters.begin(), foundLetters.end(), left) == foundLetters.end()) {
+					foundLetters.push_back(left);
+				}
+			}
+		}
+
+		for (size_t i = 0; i < word.size(); i++) {
+			cout << word[i];
+			outputFile << word[i];
+		}
+		word.clear();
+		if (foundLetters.size() > 0) {
+			cout << '(';
+			outputFile << '(';
+			for (size_t i = 0; i < foundLetters.size(); i++) {
+				cout << foundLetters[i];
+				outputFile << foundLetters[i];
+			}
+
+			cout << ')';
+			outputFile << ')';
+
+			foundLetters.clear();
+		}
+	}
+}
+
+int main() {
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+
+	ifstream inputFile("input.txt");
+	ofstream outputFile("output.txt");
+
+	char c;
+	vector<char> word;
+	vector<char> foundLetters;
+
+	while (!inputFile.eof()) {
+		inputFile.get(c);
+		if (isLetter(c)) {
+			word.push_back(c);
+		}
+		else {
+			write(word, foundLetters, outputFile);
+			cout << c;
+			outputFile << c;
+		}
+		inputFile.peek();
+	}
+	cout << "Wait...\n";
+	write(word, foundLetters, outputFile);
+
+	cout << "Completed";
+
+	return 0;
+}
